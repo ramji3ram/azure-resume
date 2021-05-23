1330ada9e26893fa7ca44576b06c3a92cacc330b
ramji2406 <ramji2406@gmail.com>
2021-05-23 14:11:15 +0530

    Connected APi with Index.html

--- frontend/main.js
+++ frontend/main.js
@@ -1,11 +1,11 @@
 window.addEventListener('DOMContentLoaded',(event) => {getVisitCount();})
 
-const functionapi = '';
+const functionapi = 'http://localhost:7071/api/GetAzureResume';
 
 const getVisitCount = () => {
     let count = 30
     fetch(functionapi).then(resp => {return resp.json()}).then(resp => {
-        console.log("web call");
+        console.log("web call"); 
         count = resp.count;
         document.getElementById("counter").innerText = count    }).catch(function (error) {
             console.log(error);
