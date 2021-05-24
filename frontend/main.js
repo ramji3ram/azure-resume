2caa19d04f1cd8d81c15fde591f2739674c53a4a
ramji2406 <ramji2406@gmail.com>
2021-05-24 09:19:01 +0530

    Deployed to production

--- frontend/main.js
+++ frontend/main.js
@@ -1,10 +1,11 @@
 window.addEventListener('DOMContentLoaded',(event) => {getVisitCount();})
 
+const functionconnectionstrinapi = "https://az2406.azurewebsites.net/api/GetAzureResume?code=qvwkLoZpVUpR/71/sF7aV9GmKAjnqnfCJSx/d8Dv6TIUDRZfEmMGgA==";
 const functionapi = 'http://localhost:7071/api/GetAzureResume';
 
 const getVisitCount = () => {
     let count = 30
-    fetch(functionapi).then(resp => {return resp.json()}).then(resp => {
+    fetch(functionconnectionstrinapi).then(resp => {return resp.json()}).then(resp => {
         console.log("web call"); 
         count = resp.count;
         document.getElementById("counter").innerText = count    }).catch(function (error) {
